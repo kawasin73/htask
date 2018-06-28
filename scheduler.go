de73bad20912f534d1273f8b7875e32e5a2b2f92
Kawamura Shintaro <kawasin73@gmail.com>
2018-06-28 22:40:01 +0900

    Refactor and create schedulerState

--- scheduler.go
+++ scheduler.go
@@ -74,80 +74,91 @@ func (c *Scheduler) Set(chCancel <-chan struct{}, t time.Time, task func(time.Ti
 	}
 }
 
-func (c *Scheduler) scheduler(wg *sync.WaitGroup, workers int) {
-	defer wg.Done()
-	// no limited min heap
-	// TODO: use limited heap
-	h := newMinHeap(0)
+type scheduleState struct {
+	heap    *minHeap
+	job     job
+	chWork  chan<- job
+	timer   *time.Timer
+	expired bool
+}
+
+func newScheduleState(heapSize int) *scheduleState {
 	timer := time.NewTimer(time.Second)
 	if !timer.Stop() {
 		<-timer.C
 	}
-	var j job
-	var chWork chan<- job
+	return &scheduleState{
+		heap:    newMinHeap(heapSize),
+		timer:   timer,
+		expired: true,
+	}
+}
+
+func (s *scheduleState) add(newJob job) error {
+	if err := s.heap.add(newJob); err != nil {
+		return err
+	}
+	if !s.expired && !s.timer.Stop() {
+		<-s.timer.C
+	}
+	s.job = s.heap.peek()
+	s.chWork = nil
+	// s.job must not be empty
+	s.timer.Reset(s.job.t.Sub(time.Now()))
+	s.expired = false
+	return nil
+}
+
+func (s *scheduleState) next() {
+	if !s.expired && !s.timer.Stop() {
+		<-s.timer.C
+	}
+	_ = s.heap.pop()
+	s.job = s.heap.peek()
+	s.chWork = nil
+	if s.job.t.IsZero() {
+		s.expired = true
+	} else {
+		s.timer.Reset(s.job.t.Sub(time.Now()))
+		s.expired = false
+	}
+}
+
+func (s *scheduleState) time(t time.Time, chWork chan<- job) {
+	s.expired = true
+	s.chWork = chWork
+	s.job.t = t
+}
+
+func (c *Scheduler) scheduler(wg *sync.WaitGroup, workers int) {
+	defer wg.Done()
+	// no limited min heap
+	// TODO: use limited heap
+	state := newScheduleState(0)
 	for {
 		select {
 		case <-c.chClose:
 			return
 		case workers = <-c.chWorkers:
-			if workers == 0 && chWork != nil {
-				go j.task(j.t)
-				chWork = nil
-				_ = h.pop()
-				j = h.peek()
-				if !j.t.IsZero() {
-					timer.Reset(j.t.Sub(time.Now()))
-				}
+			if workers == 0 && state.chWork != nil {
+				go state.job.task(state.job.t)
+				state.next()
 			}
 		case newJob := <-c.chJob:
-			if err := h.add(newJob); err != nil {
+			if err := state.add(newJob); err != nil {
 				// TODO: heap is unlimited then no error will occur
 				panic(err)
 			}
-			if chWork == nil && !j.t.IsZero() && !timer.Stop() {
-				<-timer.C
-			}
-			j = h.peek()
-			chWork = nil
-			timer.Reset(j.t.Sub(time.Now()))
-		case <-j.chCancel:
-			if chWork == nil && !j.t.IsZero() && !timer.Stop() {
-				<-timer.C
-			}
-			_ = h.pop()
-			j = h.peek()
-			chWork = nil
-			if !j.t.IsZero() {
-				timer.Reset(j.t.Sub(time.Now()))
-			}
-		case t := <-timer.C:
+		case <-state.job.chCancel:
+			state.next()
+		case t := <-state.timer.C:
+			state.time(t, c.chWork)
 			if workers == 0 {
-				for !j.t.IsZero() && !j.t.After(t) {
-					j.t = t
-					go j.task(j.t)
-					_ = h.pop()
-					j = h.peek()
-				}
-				if !j.t.IsZero() {
-					timer.Reset(j.t.Sub(time.Now()))
-				}
-			} else {
-				j.t = t
-				chWork = c.chWork
+				go state.job.task(state.job.t)
+				state.next()
 			}
-		case chWork <- j:
-			prev := j.t
-			_ = h.pop()
-			j = h.peek()
-			if j.t.IsZero() {
-				chWork = nil
-			} else if j.t.After(prev) {
-				chWork = nil
-				timer.Reset(j.t.Sub(time.Now()))
-			} else {
-				// enqueue to worker
-				j.t = prev
-			}
+		case state.chWork <- state.job:
+			state.next()
 		}
 	}
 }
